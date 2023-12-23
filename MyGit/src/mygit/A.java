6c9d356a4509239604747c35924ff5cd5c08eda7
choejiho <choejiho@localhost>
2023-12-23 16:38:55 +0900

    Master branch second edit

--- MyGit/src/mygit/A.java
+++ MyGit/src/mygit/A.java
@@ -4,6 +4,6 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("This is the 2nd version");
-		System.out.println("My changed in master branch");
+		System.out.println("My first changed in master branch");
 	}
 }
